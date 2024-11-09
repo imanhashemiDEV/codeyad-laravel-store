b6ceaf0ec562d350ab908ddb7508166115a48f77
iman hashemi <hashemi.iman@gmail.com>
2024-11-09 20:17:59 +0330

    colors

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@
 use App\Livewire\Admin\Brands\BrandList;
 use App\Livewire\Admin\Categories\CategoryList;
 use App\Livewire\Admin\Categories\TrashedCategoryList;
+use App\Livewire\Admin\Colors\ColorList;
 use App\Livewire\Admin\Panel;
 use App\Livewire\Admin\Users\UserList;
 use Illuminate\Support\Facades\Route;
@@ -19,3 +20,6 @@ Route::get('/trashed_categories', TrashedCategoryList::class)->name('admin.trash
 
 //--- brands ---//
 Route::get('/brands', BrandList::class)->name('admin.brands.list');
+
+//--- colors ---//
+Route::get('/colors', ColorList::class)->name('admin.colors.list');
