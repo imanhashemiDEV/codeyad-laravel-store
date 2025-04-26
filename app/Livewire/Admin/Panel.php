f5adad7812e414a0b33359c18dd8f290c80682a0
iman hashemi <hashemi.iman@gmail.com>
2025-04-26 23:30:31 +0330

    charts

--- app/Livewire/Admin/Panel.php
+++ app/Livewire/Admin/Panel.php
@@ -2,6 +2,11 @@
 
 namespace App\Livewire\Admin;
 
+use App\Enums\OrderDetailStatus;
+use App\Enums\OrderStatus;
+use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
@@ -9,6 +14,31 @@ use Livewire\Component;
 
 class Panel extends Component
 {
+    public $users_count=0;
+    public $in_progress_count=0;
+    public $send_count=0;
+    public $rejected_count=0;
+    public function mount(): void
+    {
+      $this->users_count = User::query()->count();
+
+      $orders = Order::query()->where('status',OrderStatus::Successful->value)->get();
+
+      foreach ($orders as $order) {
+          $order_details = OrderDetail::query()->where('order_id',$order->id)->get();
+          foreach ($order_details as $order_detail) {
+              if($order_detail->status==OrderDetailStatus::InProgress->value){
+                  $this->in_progress_count++;
+              }elseif($order_detail->status==OrderDetailStatus::Send->value){
+                  $this->send_count++;
+              }elseif($order_detail->status==OrderDetailStatus::Rejected->value){
+                  $this->rejected_count++;
+              }
+          }
+      }
+
+
+    }
     #[Layout('admin.master'),Title('پنل مدیریت')]
     public function render():View
     {
--- resources/views/livewire/admin/panel.blade.php
+++ resources/views/livewire/admin/panel.blade.php
@@ -3,7 +3,7 @@
         <!-- Users Visit -->
         <div class="panel bg-gradient-to-r from-cyan-500 to-cyan-400">
             <div class="flex justify-between">
-                <div class="font-semibold text-md ltr:mr-1 rtl:ml-1">بازدید کاربران</div>
+                <div class="font-semibold text-md ltr:mr-1 rtl:ml-1">تعداد کاربران</div>
                 <div x-data="dropdown" @click.outside="open = false" class="dropdown">
                     <a href="javascript:;" @click="toggle">
                         <svg
@@ -14,56 +14,22 @@
                             xmlns="http://www.w3.org/2000/svg"
                             class="w-5 h-5 opacity-70 hover:opacity-80"
                         >
-                            <circle cx="5" cy="12" r="2" stroke="currentColor" stroke-width="1.5" />
-                            <circle opacity="0.5" cx="12" cy="12" r="2" stroke="currentColor" stroke-width="1.5" />
-                            <circle cx="19" cy="12" r="2" stroke="currentColor" stroke-width="1.5" />
+                            <circle cx="5" cy="12" r="2" stroke="currentColor" stroke-width="1.5"/>
+                            <circle opacity="0.5" cx="12" cy="12" r="2" stroke="currentColor" stroke-width="1.5"/>
+                            <circle cx="19" cy="12" r="2" stroke="currentColor" stroke-width="1.5"/>
                         </svg>
                     </a>
-                    <ul
-                        x-cloak
-                        x-show="open"
-                        x-transition
-                        x-transition.duration.300ms
-                        class="text-black ltr:right-0 rtl:left-0 dark:text-white-dark"
-                    >
-                        <li><a href="javascript:;" @click="toggle">مشاهده گزارش</a></li>
-                        <li><a href="javascript:;" @click="toggle">ویرایش گزارش</a></li>
-                    </ul>
                 </div>
             </div>
             <div class="flex items-center mt-5">
-                <div class="text-3xl font-bold ltr:mr-3 rtl:ml-3">$170.46</div>
-                <div class="badge bg-white/30">+ 2.35%</div>
-            </div>
-            <div class="flex items-center mt-5 font-semibold">
-                <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="w-5 h-5 ltr:mr-2 rtl:ml-2"
-                >
-                    <path
-                        opacity="0.5"
-                        d="M3.27489 15.2957C2.42496 14.1915 2 13.6394 2 12C2 10.3606 2.42496 9.80853 3.27489 8.70433C4.97196 6.49956 7.81811 4 12 4C16.1819 4 19.028 6.49956 20.7251 8.70433C21.575 9.80853 22 10.3606 22 12C22 13.6394 21.575 14.1915 20.7251 15.2957C19.028 17.5004 16.1819 20 12 20C7.81811 20 4.97196 17.5004 3.27489 15.2957Z"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                    ></path>
-                    <path
-                        d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                    ></path>
-                </svg>
-                هفته گذشته 44,700
+                <div class="text-3xl font-bold ltr:mr-3 rtl:ml-3">{{$users_count}}</div>
             </div>
         </div>
 
         <!-- Sessions -->
         <div class="panel bg-gradient-to-r from-violet-500 to-violet-400">
             <div class="flex justify-between">
-                <div class="font-semibold text-md ltr:mr-1 rtl:ml-1">نشست ها</div>
+                <div class="font-semibold text-md ltr:mr-1 rtl:ml-1">سفارش در حال پردازش</div>
                 <div x-data="dropdown" @click.outside="open = false" class="dropdown">
                     <a href="javascript:;" @click="toggle">
                         <svg
@@ -74,56 +40,22 @@
                             xmlns="http://www.w3.org/2000/svg"
                             class="w-5 h-5 opacity-70 hover:opacity-80"
                         >
-                            <circle cx="5" cy="12" r="2" stroke="currentColor" stroke-width="1.5" />
-                            <circle opacity="0.5" cx="12" cy="12" r="2" stroke="currentColor" stroke-width="1.5" />
-                            <circle cx="19" cy="12" r="2" stroke="currentColor" stroke-width="1.5" />
+                            <circle cx="5" cy="12" r="2" stroke="currentColor" stroke-width="1.5"/>
+                            <circle opacity="0.5" cx="12" cy="12" r="2" stroke="currentColor" stroke-width="1.5"/>
+                            <circle cx="19" cy="12" r="2" stroke="currentColor" stroke-width="1.5"/>
                         </svg>
                     </a>
-                    <ul
-                        x-cloak
-                        x-show="open"
-                        x-transition
-                        x-transition.duration.300ms
-                        class="text-black ltr:right-0 rtl:left-0 dark:text-white-dark"
-                    >
-                        <li><a href="javascript:;" @click="toggle">مشاهده گزارش</a></li>
-                        <li><a href="javascript:;" @click="toggle">ویرایش گزارش</a></li>
-                    </ul>
                 </div>
             </div>
             <div class="flex items-center mt-5">
-                <div class="text-3xl font-bold ltr:mr-3 rtl:ml-3">74,137</div>
-                <div class="badge bg-white/30">- 2.35%</div>
-            </div>
-            <div class="flex items-center mt-5 font-semibold">
-                <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="w-5 h-5 ltr:mr-2 rtl:ml-2"
-                >
-                    <path
-                        opacity="0.5"
-                        d="M3.27489 15.2957C2.42496 14.1915 2 13.6394 2 12C2 10.3606 2.42496 9.80853 3.27489 8.70433C4.97196 6.49956 7.81811 4 12 4C16.1819 4 19.028 6.49956 20.7251 8.70433C21.575 9.80853 22 10.3606 22 12C22 13.6394 21.575 14.1915 20.7251 15.2957C19.028 17.5004 16.1819 20 12 20C7.81811 20 4.97196 17.5004 3.27489 15.2957Z"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                    ></path>
-                    <path
-                        d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                    ></path>
-                </svg>
-                هفته گذشته 84,709
+                <div class="text-3xl font-bold ltr:mr-3 rtl:ml-3">{{$in_progress_count}}</div>
             </div>
         </div>
 
         <!-- Time On-Site -->
         <div class="panel bg-gradient-to-r from-blue-500 to-blue-400">
             <div class="flex justify-between">
-                <div class="font-semibold text-md ltr:mr-1 rtl:ml-1">زمان در محل</div>
+                <div class="font-semibold text-md ltr:mr-1 rtl:ml-1">سفارش ارسال شده</div>
                 <div x-data="dropdown" @click.outside="open = false" class="dropdown">
                     <a href="javascript:;" @click="toggle">
                         <svg
@@ -134,56 +66,22 @@
                             xmlns="http://www.w3.org/2000/svg"
                             class="w-5 h-5 opacity-70 hover:opacity-80"
                         >
-                            <circle cx="5" cy="12" r="2" stroke="currentColor" stroke-width="1.5" />
-                            <circle opacity="0.5" cx="12" cy="12" r="2" stroke="currentColor" stroke-width="1.5" />
-                            <circle cx="19" cy="12" r="2" stroke="currentColor" stroke-width="1.5" />
+                            <circle cx="5" cy="12" r="2" stroke="currentColor" stroke-width="1.5"/>
+                            <circle opacity="0.5" cx="12" cy="12" r="2" stroke="currentColor" stroke-width="1.5"/>
+                            <circle cx="19" cy="12" r="2" stroke="currentColor" stroke-width="1.5"/>
                         </svg>
                     </a>
-                    <ul
-                        x-cloak
-                        x-show="open"
-                        x-transition
-                        x-transition.duration.300ms
-                        class="text-black ltr:right-0 rtl:left-0 dark:text-white-dark"
-                    >
-                        <li><a href="javascript:;" @click="toggle">مشاهده گزارش</a></li>
-                        <li><a href="javascript:;" @click="toggle">ویرایش گزارش</a></li>
-                    </ul>
                 </div>
             </div>
             <div class="flex items-center mt-5">
-                <div class="text-3xl font-bold ltr:mr-3 rtl:ml-3">38,085</div>
-                <div class="badge bg-white/30">+ 1.35%</div>
-            </div>
-            <div class="flex items-center mt-5 font-semibold">
-                <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="w-5 h-5 ltr:mr-2 rtl:ml-2"
-                >
-                    <path
-                        opacity="0.5"
-                        d="M3.27489 15.2957C2.42496 14.1915 2 13.6394 2 12C2 10.3606 2.42496 9.80853 3.27489 8.70433C4.97196 6.49956 7.81811 4 12 4C16.1819 4 19.028 6.49956 20.7251 8.70433C21.575 9.80853 22 10.3606 22 12C22 13.6394 21.575 14.1915 20.7251 15.2957C19.028 17.5004 16.1819 20 12 20C7.81811 20 4.97196 17.5004 3.27489 15.2957Z"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                    ></path>
-                    <path
-                        d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                    ></path>
-                </svg>
-                هفته گذشته 37,894
+                <div class="text-3xl font-bold ltr:mr-3 rtl:ml-3">{{$send_count}}</div>
             </div>
         </div>
 
         <!-- Bounce Rate -->
         <div class="panel bg-gradient-to-r from-fuchsia-500 to-fuchsia-400">
             <div class="flex justify-between">
-                <div class="font-semibold text-md ltr:mr-1 rtl:ml-1">بانس ریت</div>
+                <div class="font-semibold text-md ltr:mr-1 rtl:ml-1">سفارش مرجوع شده</div>
                 <div x-data="dropdown" @click.outside="open = false" class="dropdown">
                     <a href="javascript:;" @click="toggle">
                         <svg
@@ -194,52 +92,115 @@
                             xmlns="http://www.w3.org/2000/svg"
                             class="w-5 h-5 opacity-70 hover:opacity-80"
                         >
-                            <circle cx="5" cy="12" r="2" stroke="currentColor" stroke-width="1.5" />
-                            <circle opacity="0.5" cx="12" cy="12" r="2" stroke="currentColor" stroke-width="1.5" />
-                            <circle cx="19" cy="12" r="2" stroke="currentColor" stroke-width="1.5" />
+                            <circle cx="5" cy="12" r="2" stroke="currentColor" stroke-width="1.5"/>
+                            <circle opacity="0.5" cx="12" cy="12" r="2" stroke="currentColor" stroke-width="1.5"/>
+                            <circle cx="19" cy="12" r="2" stroke="currentColor" stroke-width="1.5"/>
                         </svg>
                     </a>
-                    <ul
-                        x-cloak
-                        x-show="open"
-                        x-transition
-                        x-transition.duration.300ms
-                        class="text-black ltr:right-0 rtl:left-0 dark:text-white-dark"
-                    >
-                        <li><a href="javascript:;" @click="toggle">مشاهده گزارش</a></li>
-                        <li><a href="javascript:;" @click="toggle">ویرایش گزارش</a></li>
-                    </ul>
                 </div>
             </div>
             <div class="flex items-center mt-5">
-                <div class="text-3xl font-bold ltr:mr-3 rtl:ml-3">49.10%</div>
-                <div class="badge bg-white/30">- 0.35%</div>
+                <div class="text-3xl font-bold ltr:mr-3 rtl:ml-3">{{$rejected_count}}</div>
             </div>
-            <div class="flex items-center mt-5 font-semibold">
-                <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="w-5 h-5 ltr:mr-2 rtl:ml-2"
-                >
-                    <path
-                        opacity="0.5"
-                        d="M3.27489 15.2957C2.42496 14.1915 2 13.6394 2 12C2 10.3606 2.42496 9.80853 3.27489 8.70433C4.97196 6.49956 7.81811 4 12 4C16.1819 4 19.028 6.49956 20.7251 8.70433C21.575 9.80853 22 10.3606 22 12C22 13.6394 21.575 14.1915 20.7251 15.2957C19.028 17.5004 16.1819 20 12 20C7.81811 20 4.97196 17.5004 3.27489 15.2957Z"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                    ></path>
-                    <path
-                        d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                    ></path>
-                </svg>
-                هفته گذشته 50.01%
-            </div>
+
         </div>
     </div>
 
 
+    <div class="bg-white rounded-md">
+        <div class="p-4">
+            <div id="apex_chart_one" style="height: 400px"></div>
+        </div>
+    </div>
+
 </div>
+@assets
+<script src="{{url('panel/js/apexcharts.min.js')}}"></script>
+@endassets
+@script
+<script>
+    Apex.chart = {
+        fontFamily: 'inherit',
+        locales: [{
+            "name": "fa",
+            "options": {
+                "shortDays": ["ی", "د", "س", "چ", "پ", "ج", "ش"],
+                "toolbar": {
+                    "exportToSVG": "دریافت SVG",
+                    "exportToPNG": "دریافت PNG",
+                    "menu": "فهرست",
+                    "selection": "انتخاب",
+                    "selectionZoom": "بزرگنمایی قسمت انتخاب شده",
+                    "zoomIn": "بزرگ نمایی",
+                    "zoomOut": "کوچک نمایی",
+                    "pan": "جا به جایی",
+                    "reset": "بازنشانی بزرگ نمایی"
+                }
+            }
+        }],
+        defaultLocale: "fa"
+    }
+
+    apex_chart_one();
+
+
+
+    function apex_chart_one() {
+        let options = {
+            chart: {
+                height: 350,
+                type: 'bar'
+            },
+            title: {
+                text: 'تعداد فروش در هر ماه',
+                align: 'center',
+                floating: true
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: '55%',
+                    endingShape: 'rounded'
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                show: true,
+                width: 2,
+                colors: ['transparent']
+            },
+            series: [{
+                name: 'تعداد فروش ها',
+                data: [1,2,3,4,5,6,7,8,9,9,9]
+            }],
+            xaxis: {
+                categories: ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند']
+            },
+            yaxis: {
+                title: {
+                    text: 'تعداد'
+                }
+            },
+            fill: {
+                opacity: 1
+            },
+            tooltip: {
+                y: {
+                    formatter: function (val) {
+                        return val + 'مورد';
+                    }
+                }
+            }
+        };
+
+        let chart = new ApexCharts(
+            document.querySelector("#apex_chart_one"),
+            options
+        );
+
+        chart.render();
+    }
+</script>
+@endscript
